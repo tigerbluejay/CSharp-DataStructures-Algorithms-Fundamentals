66127de3ba7fe0faa8b973e78b3574af196ecb7e
Jose Iriarte <josemariairiarte@gmail.com>
2025-01-02 14:14:20 -0300

    Initial Commit from Mac

---
+++ CSharp-DataStructures-Algorithms-Fundamentals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_DataStructures_Algorithms_Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
